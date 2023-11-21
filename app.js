03398cc13c674b79ea4eaa8194cbbf98e3fce8e6
Ryan Gogats <rgogats1@gmail.com>
2023-11-21 15:11:30 -0500

    First commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // library app for The Odin Project
+
+const myLibrary = [];
+
+function Book(title, author, is_read) {
+    this.title = title;
+    this.author = author;
+    this.is_read = is_read;
+}
+
+const TestBook = new Book('Dracula', 'Bram Stoker', true);
+
+const addBookToLibrary = (book) => {
+    myLibrary.push(book);
+};
